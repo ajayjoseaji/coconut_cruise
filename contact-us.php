3ff9808c51264517cb55db3409fa90a5ab6a09a8
ajayjoseaji <ajayjose350@gmail.com>
2023-09-07 18:23:53 +0530

    first commit

---
+++ contact-us.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from rn53themes.net/themes/demo/travelz/booking.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Sep 2022 09:04:28 GMT -->
+
+<head>
+    <title>Relax in Kerala</title>
+    <!--== META TAGS ==-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- FAV ICON -->
+    <link rel="shortcut icon" href="images/fav.png">
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins%7CQuicksand:400,500,700" rel="stylesheet">
+    <!-- FONT-AWESOME ICON CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!--== ALL CSS FILES ==-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/materialize.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/mob.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div id="preloader">
+        <div id="status">&nbsp;</div>
+    </div>
+
+    <!-- MOBILE MENU -->
+    <section>
+        <div class="ed-mob-menu">
+            <div class="ed-mob-menu-con">
+                <div class="ed-mm-left">
+                    <div class="wed-logo">
+                        <a href="index.html"><img src="images/logo.png" alt="" />
+                        </a>
+                    </div>
+                </div>
+                <div class="ed-mm-right">
+                    <div class="ed-mm-menu">
+                        <a href="#!" class="ed-micon"><i class="fa fa-bars"></i></a>
+                        <div class="ed-mm-inn">
+                            <a href="#!" class="ed-mi-close"><i class="fa fa-times"></i></a>
+                            <h4> <a href="index.html" class="mobile-a"> Home</a></h4> <br>
+
+                            <h4><a href="houseboat.html" class="mobile-a">Houseboat</a></h4><br>
+
+
+
+                            <h4><a href="contact-us.php" class="mobile-a">Contact us</a></h4><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--HEADER SECTION-->
+    <section>
+        <!-- TOP BAR -->
+        <div class="ed-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="ed-com-t1-left">
+                            <ul>
+                                <li><a href="http://relaxinkerala.net/index.html">www.relaxinkerala.net</a>
+                                </li>
+                                <li><a href="mailto:Info@relaxinkerala.net.com"> <i class="fa-solid fa-envelope"></i> :
+                                    Info@relaxinkerala.net</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="ed-com-t1-right">
+                            <ul>
+                                <li><a href="tel:+919605832999"><i class="fa-solid fa-mobile"></i> : +91 9605832999</a>
+                                </li>
+                                <li><a href="tel:+919061332999"><i class="fa-solid fa-mobile"></i> : +91 9061332999</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- LOGO AND MENU SECTION -->
+        <div class="top-logo" data-spy="affix" data-offset-top="250">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="wed-logo">
+                            <a href="index.html"><img src="images/logo.png" alt="" />
+                            </a>
+                        </div>
+                        <div class="main-menu">
+                            <ul>
+                                <li><a href="index.html">Home</a>
+                                </li>
+
+                                <li><a href="houseboat.html">Houseboat</a></li>
+                                <!--<li><a class='dropdown-button ed-sub-menu' href='#' data-activates='dropdown1'>Courses</a></li>-->
+
+                                
+                                <li><a href="contact-us.php">Contact us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!--END HEADER SECTION-->
+
+    <section>
+        <div class="rows contact-map map-container">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d125926.92201804146!2d76.363202!3d9.489906!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf431624d35e15058!2sAlleppey%20Boat%20House!5e0!3m2!1sen!2sin!4v1663676034144!5m2!1sen!2sin"
+                
+                allowfullscreen></iframe>
+        </div>
+    </section>
+    <!--====== QUICK ENQUIRY FORM ==========-->
+    <section>
+        <div class="form form-spac rows con-page">
+            <div class="container">
+                <!-- TITLE & DESCRIPTION -->
+                <div class="spe-title col-md-12">
+                    <h2><span>Contact us</span></h2>
+                    <div class="title-line">
+                        <div class="tl-1"></div>
+                        <div class="tl-2"></div>
+                        <div class="tl-3"></div>
+                    </div>
+                    <p>Book travel
+                        packages and enjoy your holidays with distinctive experience</p>
+                </div>
+
+                <div class="pg-contact">
+                    <div class="col-md-3 col-sm-6 col-xs-12 new-con new-con1">
+                        <h2>The <span>Travel</span></h2>
+                        <p>We Provide best quality food and accommodation.</p>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 new-con new-con1"> <img src="img/contact/1.html" alt="">
+                        <h4>Address</h4>
+                        <p>Chungam, Pallathuruthy, Alappuzha,Kerala - Pin 688009, S . India
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 new-con new-con3"> <img src="img/contact/2.html" alt="">
+                        <h4>CONTACT INFO:</h4>
+                        <p> <a href="tel:+919061332999" class="contact-icon">Phone: +91 9061332999</a>
+                            <br> <a href="tel:+919605832999" class="contact-icon">Mobile: +91 9605832999</a>
+                            <br> <a href="mailto:Info@relaxinkerala.net" class="contact-icon">Email: Info@relaxinkerala.net</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 new-con new-con4"> <img src="img/contact/3.html" alt="">
+                        <h4>Website</h4>
+                        <p> 
+                            <a href="https://relaxinkerala.net/">www.relaxinkerala.net</a>
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <section>
+		
+                <div class="tr-regi-form v2-search-form">
+                    <h4>Booking by <span>Email</span></h4><br>
+                    <h5>
+                                          </h5>
+                    <form class="" method="POST" action="_act/contact_enquiry.php" >
+                                <div class="alert alert-success contact__msg" style="display: none" role="alert">
+                                    Thank you for arranging a wonderful trip for us! Our team will contact you shortly!
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col-lg-12 s12">
+                                        <input type="text"  class="validate" name="name" required>
+                                        <label>Enter your name</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col-lg-6 s6">
+                                        <input type="text"  class="validate" name="phone" required>
+                                        <label>Enter your phone</label>
+                                    </div>
+                                    <div class="input-field col-lg-6 s6">
+                                        <input type="email"  class="validate" name="email" required>
+                                        <label>Enter your email</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col-lg-6 s12">
+                                        <select name="country">
+                                            <option value="" disabled selected>Select your Country</option>
+                                            <option value="Honeymoon Package">India</option>
+                                            <option value="Family Package">UAE</option>
+                                            <option value="Holiday Package">UK</option>
+                                            <option value="Group Package">USA</option>
+                                            
+                                        </select>
+                                    </div>
+                                    <div class="input-field col-lg-6 s12">
+                                        <select name="package">
+                                            <option value="" disabled selected>Select your package</option>
+                                            <option value="One Bedroom Houseboat">One Bedroom Houseboat</option>
+                                            <option value="Two Bedroom Houseboat">Two Bedroom Houseboat</option>
+                                            <option value="Three Bedroom Houseboat">Three Bedroom Houseboat</option>
+                                            
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col-lg-6 s6">
+                                        <input type="text" id="from" name="arrival" readonly>
+                                        <label for="from">Arrival Date</label>
+                                    </div>
+                                    <div class="input-field col-lg-6 s6">
+                                        <input type="text" id="to" name="departure" readonly>
+                                        <label for="to">Departure Date</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col-lg-6 s6">
+                                        <select name="noofadults">
+                                            <option value="" disabled selected>No of adults</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                            <option value="6">6</option>
+                                        </select>
+                                    </div>
+                                    <div class="input-field col-lg-6 s6">
+                                        <select name="noofchildrens">
+                                            <option value="" disabled selected>No of childrens</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                            <option value="6">6</option>											
+                                        </select>
+                                    </div>
+                                </div>							
+    
+                                <div class="row">
+                                    <div class="input-field col s6">
+                                        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+                                    </div>
+                                    							
+                                </div>							
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <input type="submit" value="Book Now" name="submit" class="waves-effect waves-light tourz-sear-btn v2-ser-btn">
+                                    </div>
+                                </div>
+                            </form>
+                </div>
+            
+        </section>
+
+
+        </div>
+    </section>
+
+    <!--DASHBOARD-->
+
+    <!--END DASHBOARD-->
+    <!--====== TIPS BEFORE TRAVEL ==========-->
+    <section>
+        <div class="rows">
+            <div class="footer">
+                <div class="container">
+                    <div class="foot-sec2">
+                        <div>
+                            <div class="row">
+                                <div class="col-sm-3 foot-spec foot-com">
+                                    <h4><span>Relax </span> in Kerala</h4>
+                                    <p>Kerala's leading tour and travels Booking website,Over 30,000 Booking worldwide.
+                                    </p>
+                                </div>
+                                <div class="col-sm-3 foot-spec foot-com">
+                                    <h4><span>Address</span> & Contact Info</h4>
+                                    <p>Chungam, Pallathuruthy,
+                                        Alappuzha,Kerala - Pin 688009, S . India</p>
+                                    <p> <span class="strong">Phone: </span> <span class="highlighted">+91 9061332999
+                                            <br>+91 9605832999</span> </p>
+                                </div>
+                                <div class="col-sm-3 col-md-3 foot-spec foot-com">
+                                    <h4><span>SUPPORT</span> & HELP</h4>
+                                    <ul class="two-columns">
+                                        <li> <a href="index.html">Home</a> </li>
+                                        <li> <a href="houseboat.html">Houseboat</a> </li>
+                                       
+                                        <li> <a href="contact-us.php">Contact Us</a> </li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-3 foot-social foot-spec foot-com">
+                                    <h4><span>Follow</span> with us</h4>
+                                    <p>Join the thousands of other There are many variations of passages</p>
+                                    <ul>
+                                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a> </li>
+                                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a> </li>
+                                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a> </li>
+                                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a> </li>
+                                        <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a> </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== FOOTER - COPYRIGHT ==========-->
+    <section>
+        <div class="rows copy">
+            <div class="container">
+                <p>Copyrights © 2022 RELAX IN KERALA. All Rights Reserved. Powered by <a href="https://globosoft.in/">
+                        Globosoft</a></p>
+            </div>
+        </div>
+    </section>
+    <!--<section>
+        <div class="icon-float">
+            <ul>
+                <li><a href="#" class="sh">1k <br> Share</a> </li>
+                <li><a href="#" class="fb1"><i class="fa fa-facebook" aria-hidden="true"></i></a> </li>
+                <li><a href="#" class="gp1"><i class="fa fa-google-plus" aria-hidden="true"></i></a> </li>
+                <li><a href="#" class="tw1"><i class="fa fa-twitter" aria-hidden="true"></i></a> </li>
+                <li><a href="#" class="li1"><i class="fa fa-linkedin" aria-hidden="true"></i></a> </li>
+                <li><a href="#" class="wa1"><i class="fa fa-whatsapp" aria-hidden="true"></i></a> </li>
+                <li><a href="#" class="sh1"><i class="fa fa-envelope-o" aria-hidden="true"></i></a> </li>
+            </ul>
+        </div>
+    </section>-->
+    <!--========= Scripts ===========-->
+    <script src="js/jquery-latest.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="js/mail.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+
+<!-- Mirrored from rn53themes.net/themes/demo/travelz/booking.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Sep 2022 09:04:28 GMT -->
+
+</html>
